292ab5ea53348cb94996beab8c2f39a99dbb690d
Xiaoqian Ma <marshal_mxq@hotmail.com>
2015-04-12 23:34:20 -0400

    First update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,83 @@
 import java.io.File;
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
-    int r,t,l,m,a,f; //cost parameters
+    static int r=1,t=2,l=1,m=16,a=2,f=4;    //cost parameters
+    static double[] S;
+    static int k;              //cardinality of S
+    static Node[][] matrix;    //
+    static Node[] A=new Node[5];
 
     public static void main(String[] args) {
-	// write your code here
 
-        init();
+
+        String configFile = "config.txt";
+        String queryFile = "query.txt";
+        //init(configFile);
+
         //read in input file
+        try{
+            File query = new File(queryFile);
+            Scanner in = new Scanner(query);
+
+            while(in.hasNextLine()){
+                String[] buffer = in.nextLine().split("\\s+");
+                k = buffer.length;
+                S=new double[k];
+                for(int i=0;i<k;i++){
+                    S[i]=Double.parseDouble(buffer[i]);
+                }
+
+                //initialize array A
+                A = new Node[(int)Math.pow(2,k)];
+                for(int i=0;i<A.length;i++){
+                    A[i]=new Node();
+                    A[i].bitmap=toBinary(i);
+                    for(int j=0;j<k;j++){
+                        if(A[i].bitmap[j]==1){
+                            A[i].n++;
+                            A[i].p*=S[j];
+                        }
+                    }
+                    A[i].c=logicalAndCostFunction(A[i]);
+                    if(noBranchCostFunction(A[i])<A[i].c){
+                        A[i].c=noBranchCostFunction(A[i]);
+                        A[i].b=true;
+                    }
+                }
+
+                //print Node
+                for(int i=0;i<A.length;i++){
+                    System.out.println("======Node"+i+" ==========");
+                    for(int j=0;j<A[i].bitmap.length;j++){
+                        System.out.print(A[i].bitmap[j]);
+                    }
+                    System.out.println();
+                    System.out.println("n; "+A[i].n);
+                    System.out.println("c: "+A[i].c);
+                    System.out.println("b: "+ A[i].b);
+                    System.out.println("p: "+A[i].p);
+                    //System.out.println(A[i].toString());
+                }
+                System.out.println("*************************************");
+                /*
+                ArrayList<ArrayList<Double>> result = subset(S);
+                int i=0;
+                for(ArrayList<Double> a: result){
+                    A[i].subset=a;
+                    A[i].n=  a.size();
+
+                }
+*/
+
+            }
+        }catch(Exception e){
+            System.err.println("Error: " + e.toString());
+        }
+
+
 
 
     }
@@ -22,11 +90,71 @@ public class Main {
         return 0.0;
     }
 
+    private static int[] toBinary(int n){
+        int[] result = new int[k];
+        int s,i=0;
+        while(n>0){
+            s=n%2;
+            n=n>>1;
+            result[i++]=s;
+        }
+        int temp;
+        for(int j=0;j<k/2;j++){
+            temp=result[j];
+            result[j]=result[k-1-j];
+            result[k-1-j]=temp;
+        }
+        return result;
+    }
 
+
+    private static double noBranchCostFunction(Node node){
+        //n is the "k", the number of terms stored in the node,
+        //or the "k basic terms" in the algorithm
+        double cost=0;
+        cost = node.n*r + (node.n-1)*l + node.n*f + a;
+        return cost;
+    }
+
+    private static double logicalAndCostFunction(Node node){
+        double cost=0, q=0;
+        q = (node.p<=0.5)? node.p : 1-node.p;
+        cost = node.n*r+(node.n-1)*l + node.n*f + t+ m*q + node.p*a;
+        return cost;
+    }
+
+    private static ArrayList<ArrayList<Double>> subset(double[] S){
+        if (S==null){
+            return null;
+        }
+
+        ArrayList<ArrayList<Double>> result = new ArrayList<ArrayList<Double>>();
+
+        for(int i =0;i<S.length;i++){
+            ArrayList<ArrayList<Double>> buffer = new ArrayList<ArrayList<Double>>();
+
+            for(ArrayList<Double>a:result){
+                buffer.add(new ArrayList<Double>(a));
+            }
+
+            for(ArrayList<Double> a: buffer){
+                a.add(S[i]);
+            }
+
+            ArrayList<Double> newElement = new ArrayList<Double>();
+            newElement.add(S[i]);
+            buffer.add(newElement);
+
+        }
+        result.add(new ArrayList<Double>());
+
+        return result;
+
+    }
     //assign cost parameters from configuration file
-    private static void init(){
-        File config = new File("config.txt");
-
+    private static void init(String configFile){
+        File config = new File(configFile);
+        //matching the cost parameters with those in configuration file
 
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,14 +1,26 @@
 /**
  * Created by Marshal on 4/12/15.
  */
+
+import java.util.ArrayList;
 public class Node {
-    int n;                  //The number n of basic terms in the corresponding subset
-    double p;               //the product p of the selectivities of all terms in the subset
+    int n=0;                  //The number n of basic terms in the corresponding subset
+    double p=1;               //the product p of the selectivities of all terms in the subset
     boolean b=false;        //whether the no-branch optimization was used to get the best cost, initialized to 0
-    int c;                  //current best cost c for the subset
+    double c=Integer.MAX_VALUE;                  //current best cost c for the subset
     Node L=null,R=null;     //the left child L and right child R of the subplans giving the best cost
+    int[] bitmap;
+    ArrayList<Double> subset;
 
     //Constructor
+    public Node(ArrayList<Double> incoming){
+        this.subset=incoming;
+        this.n = incoming.size();
+        for(double d:incoming)
+            p*=d;
+
+    }
+
     public Node(){
 
     }
